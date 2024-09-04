e43feb1adf8eb16c7ba78c35989ea6c02f306a90
That-GuyConnor <149274888+That-GuyConnor@users.noreply.github.com>
2024-09-04 15:34:40 -0700

    Update config.js

--- config.js
+++ config.js
@@ -42,7 +42,7 @@ const CONFIG = {
 	changeThemeByOS: true,
 
 	// Autochange by hour options (24hrs format, string must be in: hh:mm)
-	changeThemeByHour: true,
+	changeThemeByHour: false,
 	hourDarkThemeActive: '18:30',
 	hourDarkThemeInactive: '07:00',
 
